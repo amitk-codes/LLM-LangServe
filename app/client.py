c59b0134cbfb52938f876a8c181d24d608f3121b
Amit Kumar <amitkcodes@gmail.com>
2024-11-02 18:01:25 +0530

    passing urls for fetching responses from env

--- app/client.py
+++ app/client.py
@@ -1,20 +1,19 @@
 import requests
 import streamlit as st
+import os
+
+# using URLs from environment variables
+openai_url = os.getenv("OPENAI_URL", "http://localhost:8080/essay/invoke")
+llama_url = os.getenv("LLAMA_URL", "http://localhost:8080/poem/invoke")
 
 
 def fetch_openai_response(topic):
-    response = requests.post(
-        "http://localhost:8080/essay/invoke", json={"input": {"topic": topic}}
-    )
-
+    response = requests.post(openai_url, json={"input": {"topic": topic}})
     return response.json()["output"]["content"]
 
 
 def fetch_llama_response(topic):
-    response = requests.post(
-        "http://localhost:8080/poem/invoke", json={"input": {"topic": topic}}
-    )
-
+    response = requests.post(llama_url, json={"input": {"topic": topic}})
     return response.json()["output"]
 
 
